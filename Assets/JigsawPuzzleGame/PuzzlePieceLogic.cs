101b148d40dbade77aa789c81511e186cf0e87e4
Coco <chuaziyifaith@gmail.com>
2023-03-25 13:05:36 +0800

    fix: slected puzzle is now ordered properly

--- Assets/JigsawPuzzleGame/PuzzlePieceLogic.cs
+++ Assets/JigsawPuzzleGame/PuzzlePieceLogic.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PuzzlePieceLogic : MonoBehaviour
 {
@@ -11,6 +12,10 @@ public class PuzzlePieceLogic : MonoBehaviour
 
     private LogicManager logicManager;
     private string pieceId;
+
+    // related to layering
+    private SortingGroup sortingGroup;
+    private int initialOrder = 0;
 
     private void Start()
     {
@@ -23,6 +28,10 @@ public class PuzzlePieceLogic : MonoBehaviour
 
       // Set the piece unique id
       pieceId = Random.Range(0f, 1000000000f).ToString();
+
+      // get child
+      sortingGroup = GetComponent<SortingGroup>();
+      initialOrder = sortingGroup.sortingOrder;
     }
 
     private void Update()
@@ -53,5 +62,14 @@ public class PuzzlePieceLogic : MonoBehaviour
       {
         transform.position = point;
       }
+
+      // Change order in layer
+      sortingGroup.sortingOrder = 20;
+    }
+
+    private void OnMouseUp()
+    {
+      // reset the order
+      sortingGroup.sortingOrder = initialOrder;
     }
 }
