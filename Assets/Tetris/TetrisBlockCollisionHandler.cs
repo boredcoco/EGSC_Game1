9793ee3ba75077aaf7e3bb982d75096af009d628
Coco <chuaziyifaith@gmail.com>
2023-04-06 22:38:02 +0800

    fixed rotation issues

--- Assets/Tetris/TetrisBlockCollisionHandler.cs
+++ Assets/Tetris/TetrisBlockCollisionHandler.cs
@@ -8,9 +8,13 @@ public class TetrisBlockCollisionHandler : MonoBehaviour
     private GameObject parent = null;
     private ParentController parentController = null;
 
+    private Rigidbody rb;
+
     private void Start()
     {
+      rb = GetComponent<Rigidbody>();
       movementScript = GetComponent<TetrisBlockMovement>();
+
       if (transform.parent != null)
       {
         parent = transform.parent.gameObject;
@@ -29,10 +33,15 @@ public class TetrisBlockCollisionHandler : MonoBehaviour
 
     private bool checkIfFloorIsBelow()
     {
+      // Calculate absolute direction based on current rotateion
+      Vector3 worldDirection = transform.TransformDirection(Vector3.down);
+      Quaternion inverseRotation = Quaternion.Inverse(transform.rotation);
+      Vector3 absoluteDirection = inverseRotation * worldDirection;
+
       RaycastHit hit;
-      if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity))
+      if (Physics.Raycast(transform.position, absoluteDirection, out hit, Mathf.Infinity))
       {
-          if (hit.distance < 0.5f)
+          if (hit.distance < 0.5f && hit.transform.parent != gameObject.transform.parent)
           {
             return true;
           }
